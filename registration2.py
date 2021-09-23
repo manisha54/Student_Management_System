5d2b76aa150d331dd02c044af123961e7a9dabc2
uti <manisha54>
2021-09-23 09:47:27 +0545

    work on some crude operation of registration(add,clear,display,logout)

--- registration2.py
+++ registration2.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter.ttk import Treeview
 from tkinter import ttk
+from tkinter import messagebox
 import time
+import sqlite3
 
 # intro slider
 import random
@@ -56,6 +58,101 @@ def main():
     clock.place(x=0, y=0)
     tick()
 
+    # connect to database
+    conn = sqlite3.connect('Studentmanagement.db')
+    # create a cursor
+    c = conn.cursor()
+
+    """
+    c.execute('''CREATE TABLE addresses(
+           first_name text ,
+           last_name text ,
+           father_name text,
+           mother_name text,
+           address1 text,
+           gender text,
+           address2 text,
+           date_of_birth integer,
+           email text,
+           contact integer,
+           Id_number integer,
+           Branch text
+    )''')
+    
+    """
+
+    # Create submit button for databases
+    def submit():
+        conn = sqlite3.connect('Studentmanagement.db')
+
+        c = conn.cursor()
+        # insert in to table
+        c.execute(
+            "INSERT INTO addresses VALUES (:first_name, :last_name, :father_name, :mother_name, :address1, :gender, :address2, :date_of_birth, :email, :contact,:Id_number, :Branch)",
+            {
+                'first_name': first_name.get(),
+                'last_name': last_name.get(),
+                'father_name': father_name.get(),
+                'mother_name': mother_name.get(),
+                'address1': address1.get(),
+                'gender': gender_dropdown.get(),
+                'address2': address2.get(),
+                'date_of_birth': date_of_birth.get(),
+                'email': email.get(),
+                'contact': contact.get(),
+                'Id_number': Id_number.get(),
+                'Branch': Branch.get()
+            })
+        # showinfo messagebox
+        messagebox.showinfo("Adresses", "Stored Successfully")
+
+        conn.commit()
+        conn.close()
+
+    def clear():
+        # clear the text boxes
+        first_name.delete(0, END)
+        last_name.delete(0, END)
+        father_name.delete(0, END)
+        mother_name.delete(0, END)
+        address1.delete(0, END)
+        address2.delete(0, END)
+        date_of_birth.delete(0, END)
+        email.delete(0, END)
+        contact.delete(0, END)
+        Id_number.delete(0, END)
+        Branch.delete(0, END)
+
+
+    def query():
+        conn = sqlite3.connect('Studentmanagement.db')
+
+        c = conn.cursor()
+        # query of the database
+        c.execute("SELECT *, oid FROM addresses")
+
+        records = c.fetchall()
+        # print(records)
+
+        # Loop through the results
+        print_record = ''
+        for record in records:
+            student_table.insert('', 'end', values=(record))
+        query_label = Label(student_table, text=print_record)
+        query_label.place(x=0, y=0)
+        conn.commit()
+        conn.close()
+
+    def logout():
+        response = messagebox.askyesno("Logout Application", "Are you sure want to close this application",
+                                       icon="warning")
+        if response > 0:
+            root.destroy()
+            return
+
+
+
+
     # frames
     frame = Frame(root, bg='#8bd3dd', relief=GROOVE, borderwidth=5)
     frame.place(x=10, y=80, width=1040, height=420)
@@ -77,29 +174,41 @@ def main():
     scroll_x = Scrollbar(showentryframe2, orient=HORIZONTAL)
     scroll_y = Scrollbar(showentryframe2, orient=VERTICAL)
     student_table = Treeview(showentryframe2, columns=(
-    'No', 'First Name', 'Last Name', 'Father Name', 'Mother Name', 'Permanent Address', 'Gender',
-    'Temporary Address', 'D.O.B', 'Email Address', 'Contact Number', 'ID Number', 'Branch'),
+         'First Name', 'Last Name', 'Father Name', 'Mother Name', 'Permanent Address', 'Gender',
+        'Temporary Address', 'D.O.B', 'Email Address', 'Contact Number', 'ID Number', 'Branch'),
                              yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set)
     scroll_x.pack(side=BOTTOM, fill=X)
     scroll_y.pack(side=RIGHT, fill=Y)
     scroll_x.config(command=student_table.xview)
     scroll_y.config(command=student_table.yview)
-    student_table.heading('No', text='No')
-    student_table.heading('First Name', text='First Name')
-    student_table.heading('Last Name', text='Last Name')
-    student_table.heading('Father Name', text='Father Name')
-    student_table.heading('Mother Name', text='Mother Name')
-    student_table.heading('Permanent Address', text='Permanent Address')
-    student_table.heading('Gender', text='Gender')
-    student_table.heading('Temporary Address', text='Temporary Address')
-    student_table.heading('D.O.B', text='D.O.B')
-    student_table.heading('Email Address', text='Email Address')
-    student_table.heading('Contact Number', text='Contact Number')
-    student_table.heading('ID Number', text='ID Number')
-    student_table.heading('Branch', text='Branch')
+    #student_table.heading('No', text='No')
+    student_table.heading('First Name', text='First Name', anchor=W)
+    student_table.heading('Last Name', text='Last Name', anchor=W)
+    student_table.heading('Father Name', text='Father Name', anchor=W)
+    student_table.heading('Mother Name', text='Mother Name', anchor=W)
+    student_table.heading('Permanent Address', text='Permanent Address', anchor=W)
+    student_table.heading('Gender', text='Gender', anchor=W)
+    student_table.heading('Temporary Address', text='Temporary Address', anchor=W)
+    student_table.heading('D.O.B', text='D.O.B', anchor=W)
+    student_table.heading('Email Address', text='Email Address', anchor=W)
+    student_table.heading('Contact Number', text='Contact Number', anchor=W)
+    student_table.heading('ID Number', text='ID Number', anchor=W)
+    student_table.heading('Branch', text='Branch', anchor=W)
     student_table['show'] = 'headings'
 
-    student_table.column('No', width=50)
+   # student_table.column('No', width=50)
+    student_table.column('#0', stretch=NO, minwidth=0, width=0)
+    student_table.column('#1', stretch=NO, minwidth=0, width=100)
+    student_table.column('#2', stretch=NO, minwidth=0, width=150)
+    student_table.column('#3', stretch=NO, minwidth=0, width=150)
+    student_table.column('#4', stretch=NO, minwidth=0, width=150)
+    student_table.column('#5', stretch=NO, minwidth=0, width=180)
+    student_table.column('#6', stretch=NO, minwidth=0, width=150)
+    student_table.column('#7', stretch=NO, minwidth=0, width=200)
+    student_table.column('#8', stretch=NO, minwidth=0, width=150)
+    student_table.column('#9', stretch=NO, minwidth=0, width=150)
+    student_table.column('#10', stretch=NO, minwidth=0, width=150)
+    student_table.column('#11', stretch=NO, minwidth=0, width=150)
 
     student_table.pack(fill=BOTH, expand=1)
 
@@ -140,7 +249,7 @@ def main():
                         fg='white')
     search_btn.place(x=620, y=2, heigh=30)
     show_btn = Button(Buttondataframe2, font=('calibri', 14, 'bold'), width=10, text="SHOW ALL", bg='#FF1493',
-                      fg='white')
+                      fg='white', command=query)
     show_btn.place(x=750, y=2, heigh=30)
 
     # drop down menu for serching option
@@ -148,7 +257,7 @@ def main():
     option_dropdown = StringVar()
     option_dropdown.set("select option")
     drop2 = OptionMenu(Buttondataframe2, option_dropdown, *option)
-    drop2.config(font=('calibri', 14, 'bold'))
+    drop2.config(font=('Times', 14, 'italic '))
     drop2.place(x=260, y=3, width=140)
 
     # entry
@@ -183,23 +292,31 @@ def main():
     gender_dropdown = StringVar()
     gender_dropdown.set("select gender")
     drop1 = OptionMenu(Dataentryframe, gender_dropdown, *gender)
+    drop1.config(font=('Times', 14, 'italic '))
     drop1.place(x=650, y=145, width=160)
 
     # Buttonfor student details
-    submit_btn = Button(Buttondataframe1, font=('calibri', 16, 'bold'), width=10, text="SAVE", bg='#FF1493', fg='white')
+    submit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), relief=RIDGE, width=10, text="SAVE",
+                        bg='#FF1493', fg='white', command=submit)
     submit_btn.place(x=20, y=30)
-    query_btn = Button(Buttondataframe1, font=('calibri', 16, 'bold',), width=10, text="DISPLAY", bg='#FF1493',
-                       fg='white')
-    query_btn.place(x=20, y=90)
-    edit_btn = Button(Buttondataframe1, font=('calibri', 16, 'bold'), width=10, text="UPDATE", bg='#FF1493', fg='white')
+
+    clear_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="CLEAR", bg='#FF1493',
+                       fg='white', command=clear)
+    clear_btn.place(x=20, y=90)
+
+    edit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="UPDATE", bg='#FF1493', fg='white')
+
     edit_btn.place(x=20, y=150)
-    delete_box = Button(Buttondataframe1, font=('calibri', 16, 'bold'), width=10, text="DELETE", bg='#FF1493',
+
+    delete_box = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="DELETE", bg='#FF1493',
                         fg='white')
     delete_box.place(x=20, y=210)
-    logout_button = Button(Buttondataframe1, font=('calibri', 16, 'bold'), width=10, text="LOGOUT", bg='#FF1493',
-                           fg='white')
-    logout_button.place(x=20, y=270)
+
+    logout_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="LOGOUT", bg='#FF1493',
+                        fg='white', command=logout)
+    logout_btn.place(x=20, y=270)
 
     root.mainloop()
+
 
 
