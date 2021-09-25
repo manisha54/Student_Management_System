298b640c2f4e4d05a9c75aa1ced0bcd6bf81fb9e
uti <manisha54>
2021-09-25 15:48:53 +0545

    adding images

--- Login.py
+++ Login.py
@@ -67,15 +67,14 @@ def register():
     global screen1
     screen1 = Toplevel(screen)
     screen1.title("Register")
-    screen1.geometry("400x550")
+    screen1.iconbitmap("student.ico")
+    screen1.geometry("730x500")
+    screen1.resizable(False, False)
 
     global bg2
     bg2 = PhotoImage(file="image1.png")
     label2 = Label(screen1, image=bg2)
-    label2.place(x=0, y=0)
-
-    frame3 = Frame(screen1, bd=3, width=0, bg='#33272a', padx=20, relief=RIDGE)
-    frame3.place(x=50, y=30, width=300, height=450)
+    label2.place(x=-5, y=0)
 
     global username
     global password
@@ -85,59 +84,56 @@ def register():
     username = StringVar()
     password = StringVar()
     confirmpassword = StringVar()
-    Label(frame3, text="REGISTER HERE", fg="red", bg='#33272a', font=("francisco", 23)).place(x=0, y=20)
-    Label(frame3, text="").pack()
-    Label(frame3, text="Username  ", fg="red", bg='#33272a', font=("francisco", 18)).place(x=65, y=90)
-    username_entry = Entry(frame3, width=30, textvariable=username)
-    username_entry.place(x=30, y=130, height=30, width=200)
-    Label(frame3, text="Password  ", fg="red", bg='#33272a', font=("francisco", 18)).place(x=65, y=170)
-    password_entry = Entry(frame3, width=30, textvariable=password)
-    password_entry.place(x=30, y=210, height=30, width=200)
-    Label(frame3, text="Confirm Password  ", fg="red", bg='#33272a', font=("francisco", 18)).place(x=35, y=250)
-    password_entry2 = Entry(frame3, width=30, textvariable=confirmpassword)
-    password_entry2.place(x=30, y=290, height=30, width=200)
-    Label(frame3, text="").pack()
-    Button(frame3, text="Register", width=10, height=1, fg="blue", font=("francisco", 19),
-           command=register_user).place(x=45, y=370, height=40)
+    Label(screen1, text="REGISTER HERE!!", fg="#800080", bg='white', font=("francisco", 23)).place(x=350, y=150)
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username  ", fg="#800080", bg='white', font=("francisco", 18)).place(x=270, y=220)
+    username_entry = Entry(screen1, width=25, borderwidth='5', textvariable=username)
+    username_entry.place(x=480, y=220, height=30, width=200)
+    Label(screen1, text="Password  ", fg="#800080", bg='white', font=("francisco", 18)).place(x=270, y=260)
+    password_entry = Entry(screen1, width=25, borderwidth='5', textvariable=password)
+    password_entry.place(x=480, y=260, height=30, width=200)
+    Label(screen1, text="Confirm Password  ", fg="#800080", bg='white', font=("francisco", 18)).place(x=270, y=310)
+    password_entry2 = Entry(screen1, width=25, borderwidth='5', textvariable=confirmpassword)
+    password_entry2.place(x=480, y=310, height=30, width=200)
+    Label(screen1, text="").pack()
+    Button(screen1, text="Register", width=10, fg="#800080", font=("francisco", 19),
+           command=register_user).place(x=350, y=370, height=40)
 
 
 def login():
     global screen2
     screen2 = Toplevel(screen)
     screen2.title("Login")
-    screen2.geometry("400x550")
+    screen2.iconbitmap("student.ico")
+    screen2.geometry("730x500")
+    screen2.resizable(False, False)
 
     # image
     global bg3
-    bg3 = PhotoImage(file="image1.png")
+    bg3 = PhotoImage(file="image5.png")
     label3 = Label(screen2, image=bg3)
-    label3.place(x=0, y=0)
-    frame2 = Frame(screen2, bd=3, width=0, bg='#33272a', padx=20, relief=RIDGE)
-    frame2.place(x=50, y=30, width=300, height=450)
+    label3.place(x=-5, y=0)
 
-    # global bg4
-    # bg4 = PhotoImage(file="images.png")
-    # label4 = Label(screen2, image=bg4)
-    # label4.place(x=0, y=0)
-
-    Label(frame2, text="HELLO AGAIN!", bg='#33272a', fg="red", font=("francisco", 20, "bold")).place(x=40, y=30)
-    Label(frame2, text="").pack()
+    Label(screen2, text="HELLO AGAIN!", bg='white', fg="#0000FF", font=("francisco", 20, "bold")).place(x=470, y=150)
+    Label(screen2, text="").pack()
     global username_verify
     global password_verify
     username_verify = StringVar()
     password_verify = StringVar()
     global username_entry1
     global password_entry1
-    Label(frame2, text="Username  ", bg='#33272a', fg="red", font=("francisco", 18)).place(x=80, y=100)
-    username_entry1 = Entry(frame2, width=30, textvariable=username_verify)
-    username_entry1.place(x=40, y=135, height=30)
-    Label(frame2, text="").pack()
-    Label(frame2, text="Password  ", bg='#33272a', fg="red", font=("francisco", 18)).place(x=80, y=185)
-    password_entry1 = Entry(frame2, show="*", width=30, textvariable=password_verify)
-    password_entry1.place(x=40, y=220, height=30)
+    Label(screen2, text="Username  ", bg='white', fg="#0000FF", font=("francisco", 18)).place(x=390, y=230)
+    username_entry1 = Entry(screen2, width=25, borderwidth='5', textvariable=username_verify)
+    username_entry1.place(x=530, y=230, height=30)
+    Label(screen2, text="").pack()
+    Label(screen2, text="Password  ", bg='white', fg="#0000FF", font=("francisco", 18)).place(x=390, y=280)
+    password_entry1 = Entry(screen2, show="*", width=25, borderwidth='5', textvariable=password_verify)
+    password_entry1.place(x=530, y=280, height=30)
 
-    Label(frame2, text="").pack()
-    Button(frame2, text="Login", width=10, fg="blue", font=("francisco", 19), command=login_verify).place(x=50, y=310)
+    Label(screen2, text="").pack()
+    Button(screen2, text="Login", width=10, fg="#0000FF", font=("francisco", 19), command=login_verify).place(x=480,
+                                                                                                              y=350,
+                                                                                                              height=40)
 
 
 def main_screen():
@@ -146,22 +142,33 @@ def main_screen():
     screen.geometry('1200x750+200+0')
     screen.resizable(False, False)
     screen.title('Student Management System')
+    screen.iconbitmap("image2.ico")
 
-    # image
-    global bg1
-    bg1 = PhotoImage(file="image1.png")
-    label1 = Label(screen, image=bg1)
-    label1.place(x=0, y=0)
+    frame = Frame(screen, padx=20, bg='#8bd3dd', borderwidth=0, relief=RIDGE)
+    frame.place(x=620, y=0, width=620, height=800)
 
-    frame1 = Frame(screen, padx=20, bg='#33272a', relief=RIDGE)
-    frame1.place(x=400, y=110, width=450, height=550)
+    frame1 = Frame(screen, padx=20, bg='#f9bc60', relief=RIDGE)
+    frame1.place(x=700, y=110, width=450, height=550)
+
+    frame2 = Frame(screen, padx=20, bg='#33272a', borderwidth=0, relief=RIDGE)
+    frame2.place(x=0, y=0, width=620, height=800)
+
+    global bg4
+    bg4 = PhotoImage(file="image3.png")
+    label1 = Label(frame2, image=bg4)
+    label1.place(x=-50, y=0, height=850)
 
     # Title and subtitle
-    title = Label(frame1, text='Create Admin', font=("Impact", 35, "bold"), fg='red', bg="#33272a")
+    title = Label(frame1, text='Create Admin', font=("Impact", 35, "bold"), fg='#800080', bg="#f9bc60")
     title.place(x=70, y=30)
-    subtitle = Label(frame1, text='Welcome to Student Management System', font=("Gudy old style", 14, "bold"), fg='red',
-                     bg="#33272a")
-    subtitle.place(x=10, y=100)
+    subtitle = Label(frame1, text='LOGIN AND REGISTER HERE', font=("Gudy old style", 14, "bold"), fg='#800080',
+                     bg="#f9bc60")
+    subtitle.place(x=70, y=100)
+
+    subtitle1 = Label(frame2, text='WELCOME TO STUDENT MANAGEMENT SYSTEM', font=('chiller', 27, 'italic bold'),
+                      fg='#800080',
+                      bg="#f9bc60", width=60, height=3)
+    subtitle1.place(x=-130, y=0)
 
     # textboxes
     Label(text=" ", bg='white').pack()
