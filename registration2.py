9b5c054382d2d398772037468b497f9977dbf4ae
uti <manisha54>
2021-09-27 09:53:55 +0545

    works on crude operation

--- registration2.py
+++ registration2.py
@@ -42,6 +42,7 @@ def main():
     root.config(bg='pink')
     root.geometry('1060x785+200+0')
     root.resizable(0, 0)
+    root.iconbitmap("student.ico")
 
     tt = 'Welcome to student management system'
     count = 0
@@ -135,13 +136,125 @@ def main():
         # print(records)
 
         # Loop through the results
-        print_record = ''
-        for record in records:
-            student_table.insert('', 'end', values=(record))
-        query_label = Label(student_table, text=print_record)
-        query_label.place(x=0, y=0)
+        if (len(records) != 0):
+            student_table.delete(*student_table.get_children())
+            for row in records:
+                student_table.insert('', END, values=row)
+
+
         conn.commit()
         conn.close()
+
+    def get_cursor(ev):
+        clear()
+        cursor_row = student_table.focus()
+        content = student_table.item(cursor_row)
+        row = content['values']
+
+        first_name.insert(0, row[0])
+        last_name.insert(0, row[1])
+        father_name.insert(0, row[2])
+        mother_name.insert(0, row[3])
+        address1.insert(0, row[4])
+        address2.insert(0, row[6])
+        date_of_birth.insert(0, row[7])
+        email.insert(0, row[8])
+        contact.insert(0, row[9])
+        Id_number.insert(0, row[10])
+        Branch.insert(0, row[11])
+
+    def delete():
+        conn = sqlite3.connect('Studentmanagement.db')
+
+        c = conn.cursor()
+
+        # delete a record
+        c.execute("DELETE from addresses WHERE oid = " + delete_box_entry.get())
+        messagebox.showinfo('DELETE DATA',
+                            'You have successfully deleted the data of ID number ')
+
+        # query of the database
+        c.execute("SELECT *, oid FROM addresses")
+        records = c.fetchall()
+        # print(records)
+        # Loop through the results
+        print_record = ''
+        for record in records:
+            # str(record[12]) added for displaying the id
+            print_record += str(record[12]) + ' ' + str(record[0]) + ' ' + str(record[1]) + ' ' + str(
+                record[2]) + ' ' + str(record[3]) + ' ' + str(record[4]) + ' ' + str(record[5]) + ' ' + str(
+                record[6]) + ' ' + str(record[7]) + ' ' + str(record[8]) + ' ' + str(record[9]) + ' ' + str(
+                record[10]) + ' ' + str(record[11]) + '\t' + "\n"
+
+        conn.commit()
+        conn.close()
+        clear()
+
+    def update():
+        conn = sqlite3.connect('Studentmanagement.db')
+
+        c = conn.cursor()
+        # update of the database
+        record_id = delete_box_entry.get()
+        c.execute("SELECT *FROM addresses WHERE oid=" + record_id)
+        record = c.fetchall()
+
+        for records in record:
+            first_name.insert(0, records[0])
+            last_name.insert(0, records[1])
+            father_name.insert(0, records[2])
+            mother_name.insert(0, records[3])
+            address1.insert(0, records[4])
+            address2.insert(0, records[6])
+            date_of_birth.insert(0, records[7])
+            email.insert(0, records[8])
+            contact.insert(0, records[9])
+            Id_number.insert(0, records[10])
+            Branch.insert(0, records[11])
+
+
+    def editor():
+        """this function will update the datas saved in the database."""
+        conn = sqlite3.connect('Studentmanagement.db')
+
+        c = conn.cursor()
+        record_id = delete_box_entry.get()
+        c.execute("""UPDATE addresses SET
+                first_name = :first,
+                last_name = :last,
+                father_name = :father,
+                mother_name = :mother,
+                address1 = : permanent address,
+                address2 = : temporary address,
+                date_of_birth= :dob,
+                email = :email address
+                contact = :contact number,
+                Id_number = :ID number,
+                Branch = :Branch
+                
+                WHERE oid = :oid""",
+                  {'first': first_name.get(),
+                   'last': last_name.get(),
+                   'father': father_name.get(),
+                   'mother': mother_name.get(),
+                   'permanent address': address1.get(),
+                   ' temporary address': address2.get(),
+                   'dob': date_of_birth.get(),
+                   'email address': email.get(),
+                   'contact number': contact.get(),
+                   'Id number': Id_number.get(),
+                   'Branch': Branch.get(),
+                   'oid': record_id
+                   })
+        conn.commit()
+        conn.close()
+        
+
+
+
+
+
+
 
     def logout():
         response = messagebox.askyesno("Logout Application", "Are you sure want to close this application",
@@ -175,7 +288,7 @@ def main():
     scroll_y = Scrollbar(showentryframe2, orient=VERTICAL)
     student_table = Treeview(showentryframe2, columns=(
          'First Name', 'Last Name', 'Father Name', 'Mother Name', 'Permanent Address', 'Gender',
-        'Temporary Address', 'D.O.B', 'Email Address', 'Contact Number', 'ID Number', 'Branch'),
+        'Temporary Address', 'D.O.B', 'Email Address', 'Contact Number', 'ID Number', 'Branch', 'Delete/Update ID'),
                              yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set)
     scroll_x.pack(side=BOTTOM, fill=X)
     scroll_y.pack(side=RIGHT, fill=Y)
@@ -194,6 +307,7 @@ def main():
     student_table.heading('Contact Number', text='Contact Number', anchor=W)
     student_table.heading('ID Number', text='ID Number', anchor=W)
     student_table.heading('Branch', text='Branch', anchor=W)
+    student_table.heading('Delete/Update ID', text='Delete/Update ID', anchor=W)
     student_table['show'] = 'headings'
 
    # student_table.column('No', width=50)
@@ -209,34 +323,38 @@ def main():
     student_table.column('#9', stretch=NO, minwidth=0, width=150)
     student_table.column('#10', stretch=NO, minwidth=0, width=150)
     student_table.column('#11', stretch=NO, minwidth=0, width=150)
+    student_table.column('#12', stretch=NO, minwidth=0, width=150)
 
     student_table.pack(fill=BOTH, expand=1)
+    student_table.bind("<ButtonRelease-1>", get_cursor)
 
     # label
     first_name_label = Label(Dataentryframe, text='First Name :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    first_name_label.place(x=15, y=45)
+    first_name_label.place(x=15, y=25)
     last_name_label = Label(Dataentryframe, text='Last Name :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    last_name_label.place(x=465, y=45)
+    last_name_label.place(x=465, y=25)
     father_name_label = Label(Dataentryframe, text='Father Name :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    father_name_label.place(x=15, y=95)
+    father_name_label.place(x=15, y=75)
     mother_name_label = Label(Dataentryframe, text='Mother Name :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    mother_name_label.place(x=465, y=95)
+    mother_name_label.place(x=465, y=75)
     address1_label = Label(Dataentryframe, text='Permanent Address :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    address1_label.place(x=15, y=145)
+    address1_label.place(x=15, y=125)
     gender_label = Label(Dataentryframe, text='Gender :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    gender_label.place(x=465, y=145)
+    gender_label.place(x=465, y=125)
     address2_label = Label(Dataentryframe, text='Temporary Address :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    address2_label.place(x=15, y=195)
+    address2_label.place(x=15, y=175)
     date_of_birth_label = Label(Dataentryframe, text='D.O.B :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    date_of_birth_label.place(x=465, y=195)
+    date_of_birth_label.place(x=465, y=175)
     email_label = Label(Dataentryframe, text='Email Address :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    email_label.place(x=15, y=245)
+    email_label.place(x=15, y=225)
     contact_label = Label(Dataentryframe, text='Contact Number :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    contact_label.place(x=465, y=245)
+    contact_label.place(x=465, y=225)
     Id_number_label = Label(Dataentryframe, text='ID Number :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    Id_number_label.place(x=15, y=295)
+    Id_number_label.place(x=15, y=275)
     branch_label = Label(Dataentryframe, text='Branch :', bg='#ffd803', font=('Times', 15, 'italic bold'))
-    branch_label.place(x=465, y=295)
+    branch_label.place(x=465, y=275)
+    delete_box = Label(Dataentryframe, text='Delete/Update ID :', bg='#ffd803', font=('Times', 15, 'italic bold'))
+    delete_box.place(x=15, y=325)
 
     # for searching
     search_label = Label(Buttondataframe2, text='Search By', bg='#8bd3dd', font=('Times', 17, 'italic bold'))
@@ -262,29 +380,31 @@ def main():
 
     # entry
     first_name = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    first_name.place(x=250, y=45)
+    first_name.place(x=250, y=25)
     last_name = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    last_name.place(x=650, y=45)
+    last_name.place(x=650, y=25)
     father_name = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    father_name.place(x=250, y=95)
+    father_name.place(x=250, y=75)
     mother_name = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    mother_name.place(x=650, y=95)
+    mother_name.place(x=650, y=75)
     address1 = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    address1.place(x=250, y=145)
+    address1.place(x=250, y=125)
     # gender = Entry( Dataentryframe, font = ('Times', 14 ,'italic ' ), width = 15, borderwidth = 5)
     # gender.place(x =650 , y = 145)
     address2 = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    address2.place(x=250, y=195)
+    address2.place(x=250, y=175)
     date_of_birth = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    date_of_birth.place(x=650, y=195)
+    date_of_birth.place(x=650, y=175)
     email = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    email.place(x=250, y=245)
+    email.place(x=250, y=225)
     contact = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    contact.place(x=650, y=245)
+    contact.place(x=650, y=225)
     Id_number = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    Id_number.place(x=250, y=295)
+    Id_number.place(x=250, y=275)
     Branch = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=15, borderwidth=5)
-    Branch.place(x=650, y=295)
+    Branch.place(x=650, y=275)
+    delete_box_entry = Entry(Dataentryframe, font=('Times', 14, 'italic '), width=5, borderwidth=5)
+    delete_box_entry.place(x=250, y=325)
 
     # dropdown
     # drop down menu for gender
@@ -293,30 +413,35 @@ def main():
     gender_dropdown.set("select gender")
     drop1 = OptionMenu(Dataentryframe, gender_dropdown, *gender)
     drop1.config(font=('Times', 14, 'italic '))
-    drop1.place(x=650, y=145, width=160)
+    drop1.place(x=650, y=125, width=160)
 
     # Buttonfor student details
-    submit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), relief=RIDGE, width=10, text="SAVE",
+    submit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), relief=RIDGE, width=13, text="SUBMIT",
                         bg='#FF1493', fg='white', command=submit)
-    submit_btn.place(x=20, y=30)
+    submit_btn.place(x=5, y=15, height=40)
 
-    clear_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="CLEAR", bg='#FF1493',
+    query_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=13, text="CLEAR", bg='#FF1493',
                        fg='white', command=clear)
-    clear_btn.place(x=20, y=90)
+    query_btn.place(x=5, y=75, height=40)
 
-    edit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="UPDATE", bg='#FF1493', fg='white')
+    edit_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=13, text="UPDATE", bg='#FF1493', fg='white',command=update)
 
-    edit_btn.place(x=20, y=150)
+    edit_btn.place(x=5, y=135, height=40)
 
-    delete_box = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="DELETE", bg='#FF1493',
-                        fg='white')
-    delete_box.place(x=20, y=210)
+    save_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=13, text="SAVE", bg='#FF1493',
+                      fg='white', command=editor)
+    save_btn.place(x=5, y=195, height=40)
 
-    logout_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=10, text="LOGOUT", bg='#FF1493',
+    delete_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=13, text="DELETE", bg='#FF1493',
+                        fg='white', command=delete)
+    delete_btn.place(x=5, y=255, height=40)
+
+    logout_btn = Button(Buttondataframe1, font=('chiller', 20, 'bold'), width=13, text="LOGOUT", bg='#FF1493',
                         fg='white', command=logout)
-    logout_btn.place(x=20, y=270)
+    logout_btn.place(x=5, y=315, height=40)
+
 
     root.mainloop()
 
 
-
+main()
